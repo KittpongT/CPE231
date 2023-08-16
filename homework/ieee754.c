2ced04568f32e736695e71891815935177a37e73
Inteoty <kittipong.tpy@gmail.com>
2023-08-16 19:38:10 +0700

    created converted function

--- homework/ieee754.c
+++ homework/ieee754.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
+char *base2FromInt(int integer){
+    return "Hello Wrold";
+}
+
+char *base2FromFrac(double fraction){
+
+    return "Hello World";
+}
+
+void convertToIEEE754(double decimal){
+    int intPart = (int) decimal;
+    double fracPart = decimal - intPart;
+
+    char *intBase2 = base2FromInt(intPart);
+    char *fracBase2 = base2FromFrac(fracPart);
+}
+
 int main(){
+
+    double decimal;
+    scanf("%lf", &decimal);
+
+    convertToIEEE754(decimal);
+
+
     return 0;
 }
