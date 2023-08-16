3b87de4b0b142d0df21683052a3c47ce036a974b
Inteoty <kittipong.tpy@gmail.com>
2023-08-16 18:06:59 +0700

    add selection sort

--- sort/selection_sort.c
+++ sort/selection_sort.c
@@ -3,7 +3,30 @@
 int main(){
     int n;
     scanf("%d", &n);
-
-    printf("Hello World");
     int arr[n];
+
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n-1; i++){
+        int min = i;
+        
+        for (int j = i+1; j < n; j++)
+        {
+            if(arr[j] < arr[min]){
+                min = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+        
+    }
+
+    for (int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
+    
 }
